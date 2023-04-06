21f46c6176591b0a0e432a9bd23cb9b8fcc8e43d
Robert Sears <rob@MacBook-Air-10.home>
2023-04-06 15:40:22 -0500

    Refactoring

--- lua/NeoView.lua
+++ lua/NeoView.lua
@@ -58,7 +58,7 @@ end
 function NeoView.restore_view()
   if NeoView.valid_buffer() then
     cmd('silent! loadview')
-    NeoView.restore_cursor_position()
+    vim.schedule(NeoView.restore_cursor_position)
   end
 end
 
