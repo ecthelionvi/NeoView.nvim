eb377619a10e307b0d38aa4e9aa3564dde3516fb
Robert Sears <rob@MacBook-Air-10.home>
2023-04-05 22:32:37 -0500

    Refactoring

--- lua/NeoView.lua
+++ lua/NeoView.lua
@@ -66,7 +66,7 @@ end
 function NeoView.validate_buffer()
   local buftype = vim.bo.buftype
   local disabled = { "terminal", "nofile" }
-  return vim.tbl_contains(disabled, buftype) and false or true
+  if not vim.tbl_contains(disabled, buftype) then return true end
 end
 
 -- Save-Cursor-Position
